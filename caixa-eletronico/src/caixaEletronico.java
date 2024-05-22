681f18d73939bb936d37119c8892a3fdf236b2dc
MarcioFelipe02 <marciofelipe02@outlook.com>
2024-05-22 01:13:35 -0300

    caixa eletronico

---
+++ caixa-eletronico/src/caixaEletronico.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class caixaEletronico {
+    public static void main(String[] args) throws Exception {
+        double saldo = 300.75;
+        double valorSolicitado;
+
+        valorSolicitado = Double.parseDouble(JOptionPane.showInputDialog("Favor informar o valor do saque"));
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+             JOptionPane.showMessageDialog(null, "O valor do saldo atual é de " + saldo );
+                  
+        }else
+        JOptionPane.showMessageDialog(null,"Saldo insuficiente" );
+
+
+    }
+}
